3f7fd7e8092a449734ea647e71e0a2fa98d3b323
mrcormell <d.cormell@etoncollege.org.uk>
2021-05-08 09:22:57 +0100

    Initial Commit

---
+++ MusicByLocationSwiftUIPractice/MusicByLocationSwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationSwiftUIPracticeApp.swift
+//  MusicByLocationSwiftUIPractice
+//
+//  Created by David Cormell on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationSwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
